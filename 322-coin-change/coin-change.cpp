7a825a10c994a044547e8d4b2911d77f783bb4ee
Rohit Khandelwal <159414490+rohit2004khandelwal@users.noreply.github.com>
2025-07-18 14:24:21 +0530

    Time: 35 ms (57.49%) | Memory: 19.1 MB (60.97%) - LeetSync

---
+++ 322-coin-change/coin-change.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    //RECURSION 
+    int solve(vector<int>& coins, int amount){
+        //BASE CASE
+        if(amount == 0) return 0;
+        int minCoin = INT_MAX;
+        //1 case hum baki recursion
+        for(int i=0;i<coins.size();i++){
+            int coin = coins[i];
+            if(coin<=amount){
+                int recAns = solve(coins, amount-coin);
+                if(recAns != INT_MAX){
+                    int coinUsed = 1+recAns;
+                    minCoin = min(minCoin, coinUsed);
+                }
+            }
+        }
+        return minCoin;
+    }
+
+    //DP(TOP DOWN):-
+    int solveMem(vector<int>& coins, int amount, vector<int>&dp){
+        if(amount == 0) return 0;
+
+        if(dp[amount] != -1) return dp[amount];
+
+        int minCoin = INT_MAX;
+        //1 case hum baki recursion
+        for(int i=0;i<coins.size();i++){
+            int coin = coins[i];
+            if(coin<=amount){
+                int recAns = solveMem(coins, amount-coin, dp);
+                if(recAns != INT_MAX){
+                    int coinUsed = 1+recAns;
+                    minCoin = min(minCoin, coinUsed);
+                }
+            }
+        }
+        dp[amount]= minCoin;
+        return dp[amount];
+    }
+    
+    int coinChange(vector<int>& coins, int amount) {
+        // int ans = solve(coins, amount);
+        vector<int> dp(amount+1,-1);
+        int ans = solveMem(coins, amount, dp);
+        if(ans == INT_MAX) {
+            return -1;
+        }else{
+            return ans;
+        }
+    }
+};
