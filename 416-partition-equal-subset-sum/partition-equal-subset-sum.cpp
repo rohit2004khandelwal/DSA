4d05b2a45bcc0d234dc5ec130932b2a02fbfdba7
Rohit Khandelwal <159414490+rohit2004khandelwal@users.noreply.github.com>
2025-07-20 20:01:07 +0530

    Time: 213 ms (9.59%) | Memory: 28.4 MB (49.99%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -18,13 +18,35 @@ public:
         dp[target][i] = inc || exc;
         return dp[target][i];
     }
+    //BOTTOM UP APPROACH
+    bool solveTab(vector<int>& nums, int target){
+        int n= nums.size();
+        vector<vector<bool>> dp(target+1, vector<bool>(n+1, 0));
+        //BASE CASE ANALYSIS
+        for(int col =0;col<=nums.size();col++){
+            dp[0][col] = true;
+        }
+
+        for(int t=1;t<=target;t++){
+            for(int i=n-1;i>=0;i--){
+                bool inc = false;
+                if (t - nums[i] >= 0) {
+                     inc = dp[t - nums[i]][i + 1];
+                }
+                bool exc = dp[t][i+1];
+                dp[t][i] = inc || exc;
+            }
+        }
+        return dp[target][0];
+    }
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
         int sum = accumulate(nums.begin(),nums.end(), 0);
         if(sum%2 != 0) return false;
         int target = sum>>1;
         // return solve(nums,target,0);
-        vector<vector<int> > dp(target+1, vector<int>(n+1, -1));
-        return solveMem(nums, target, 0, dp);
+        // vector<vector<int> > dp(target+1, vector<int>(n+1, -1));
+        // return solveMem(nums, target, 0, dp);
+        return solveTab(nums,target);
     }
 };
