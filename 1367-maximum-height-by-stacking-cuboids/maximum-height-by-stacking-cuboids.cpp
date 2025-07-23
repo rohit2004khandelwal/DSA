de590ae45c29521e9c2eac79b27dcdbef7e7d305
Rohit Khandelwal <159414490+rohit2004khandelwal@users.noreply.github.com>
2025-07-23 20:25:00 +0530

    Time: 50 ms (34.56%) | Memory: 25.4 MB (27.00%) - LeetSync

--- 1367-maximum-height-by-stacking-cuboids/maximum-height-by-stacking-cuboids.cpp
+++ 1367-maximum-height-by-stacking-cuboids/maximum-height-by-stacking-cuboids.cpp
@@ -44,6 +44,25 @@ public:
         }
         return dp[0][-1+1];
     }
+    //SPACE OPT
+    int solveTabSO(vector<vector<int>>& cuboids){
+        int n= cuboids.size();
+        // vector<vector<int>> dp(n+1,vector<int>(n+1, 0));
+        vector<int>prevRow(n+1, 0);
+        vector<int>currRow(n+1, 0);
+        for(int curr=n-1;curr>=0;curr--){
+            for(int prev=curr-1;prev>=-1;prev--){
+                int inc = 0;
+                if(prev == -1 || isSafe(cuboids[prev], cuboids[curr])){
+                      inc = cuboids[curr][2] + prevRow[curr+1];//bcoz prev + 1 karna hai
+                }
+                int exc = 0 + prevRow[prev+1];
+                currRow[prev+1] = max(inc, exc);
+            }
+            prevRow = currRow;
+        }
+        return prevRow[-1+1];
+    }
     int maxHeight(vector<vector<int>>& cuboids) {
         int curr = 0;
         int prev = -1;
@@ -57,6 +76,7 @@ public:
         // return solveRec(cuboids, curr, prev);
         // vector<vector<int>> dp(n+1,vector<int>(n+1, -1));
         // return solveMem(cuboids,curr, prev, dp);
-        return solveTab(cuboids);
+        // return solveTab(cuboids);
+        return solveTabSO(cuboids);
     }
 };
