ab576bcb24f4c8e7d7437463f75ce23e27091b86
Rohit Khandelwal <159414490+rohit2004khandelwal@users.noreply.github.com>
2025-07-24 23:33:20 +0530

    Time: 27 ms (41.53%) | Memory: 14.8 MB (23.78%) - LeetSync

---
+++ 1228-minimum-cost-tree-from-leaf-values/minimum-cost-tree-from-leaf-values.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int solveRec(vector<int>&arr, map<pair<int,int>, int>&maxi, int s, int e){
+         // base case
+        if(s>=e) return 0;
+        // 1case
+        int ans = INT_MAX;
+        for(int i = s; i < e; i++){
+            int leftRangeMax = maxi[{s,i}];
+            int rightRangeMax = maxi[{i+1,e}];
+            int nonLeafValue = leftRangeMax * rightRangeMax;
+            ans = min(ans, nonLeafValue + solveRec(arr,maxi,s,i) + solveRec(arr,maxi,i+1,e));
+        }
+        return ans;
+    }
+    //TOP DOWN
+    int solveMem(vector<int>&arr, map<pair<int,int>, int>&maxi, int s, int e, vector<vector<int>>& dp){
+         // base case
+        if(s>=e) return 0;
+        if(dp[s][e] != -1) return dp[s][e];
+        // 1case
+        int ans = INT_MAX;
+        for(int i = s; i < e; i++){
+            int leftRangeMax = maxi[{s,i}];
+            int rightRangeMax = maxi[{i+1,e}];
+            int nonLeafValue = leftRangeMax * rightRangeMax;
+            ans = min(ans, nonLeafValue + solveMem(arr,maxi,s,i,dp) + solveMem(arr,maxi,i+1,e,dp));
+        }
+        dp[s][e] = ans;
+        return dp[s][e];
+    }
+    //Bottom up
+    // int solveTab()
+    int mctFromLeafValues(vector<int>& arr) {
+        map< pair<int,int>, int> maxi;
+
+        // loop for all the pairs for range
+        int n = arr.size();
+        for(int i = 0; i < n; i++){
+            for(int j = i ; j < n;j++){
+                if(i==j){ //single element
+                    maxi[{i,j}] = arr[i];
+                }
+                else{
+                    maxi[{i,j}] = max(maxi[{i,j-1}], arr[j]);
+                }
+            }
+        }
+        int s = 0;
+        int e = n - 1;
+        // return solveRec(arr,maxi,s,e);
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        return solveMem(arr,maxi,s,e,dp);
+    }
+};
