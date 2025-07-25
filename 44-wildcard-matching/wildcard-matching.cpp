680e1f15306f078b22bacbdf0ad902190f7fe0e9
Rohit Khandelwal <159414490+rohit2004khandelwal@users.noreply.github.com>
2025-07-25 13:38:04 +0530

    Time: 35 ms (43.27%) | Memory: 31.9 MB (17.11%) - LeetSync

---
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    bool solveRec(string &s, string &p, int i, int j){
+        //base case
+        if(i == s.length() && j == p.length()) return true; //sare characters match hogye hai
+        if(j == p.length()) return false;// pattern khtm hogya, but string khtm nhi hui
+        if(i == s.length()){
+            // agar sare bache hue characters * hai, then true else false
+            for(int k=j;k<p.length();k++){
+                if(p[k] != '*') return false;
+            }
+            // reached here -> sare characters * mile p string me
+            return true;
+        }
+        //MAIN LOGIC:-Match or no match
+        if(p[j] == '?' || s[i] == p[j]){ //single character match
+            return solveRec(s,p,i+1,j+1);
+        }
+         else if(p[j] == '*'){
+            // * can be empty string, or any character
+            return solveRec(s,p,i+1,j) || solveRec(s,p,i,j+1);//(i+1) mai character bna and (j+1) mai empty bna
+        }
+        else{
+            // no match
+            return false;
+        }
+
+    }
+    //TOP DOWN
+    bool solveMem(string &s, string &p, int i, int j, vector<vector<int>>& dp){
+        //base case
+        if(i == s.length() && j == p.length()) return true; //sare characters match hogye hai
+        if(j == p.length()) return false;// pattern khtm hogya, but string khtm nhi hui
+        if(i == s.length()){
+            // agar sare bache hue characters * hai, then true else false
+            for(int k=j;k<p.length();k++){
+                if(p[k] != '*') return false;
+            }
+            // reached here -> sare characters * mile p string me
+            return true;
+        }
+        if(dp[i][j] != -1) return dp[i][j];
+        //MAIN LOGIC:-Match or no match
+        bool ans;
+        if(p[j] == '?' || s[i] == p[j]){ //single character match
+            ans = solveMem(s,p,i+1,j+1,dp);
+        }
+         else if(p[j] == '*'){
+            // * can be empty string, or any character
+            ans = solveMem(s,p,i+1,j,dp) || solveMem(s,p,i,j+1,dp);//(i+1) mai character bna and (j+1) mai empty bna
+        }
+        else{
+            // no match
+            ans = false;
+        }
+        dp[i][j] = ans;
+        return dp[i][j];
+
+    }
+    bool isMatch(string s, string p) {
+        // bool ans = solveRec(s, p, 0, 0);
+        // return ans;
+        int m = s.length();
+        int n = p.length();
+        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
+        return solveMem(s, p, 0, 0, dp);
+    }
+};
