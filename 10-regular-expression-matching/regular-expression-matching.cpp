be5e51f99600f0c3220e4f529f784af4ab9a12f0
Rohit Khandelwal <159414490+rohit2004khandelwal@users.noreply.github.com>
2025-07-26 23:31:41 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (33.85%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -18,7 +18,35 @@ public:
             return false;
         }
     }
+    //TOP DOWN
+    bool solveMem(string &s, string &p, int i, int j, vector<vector<int>> &dp){
+        if(i == s.length() && j == p.length()) return true;
+        if(j == p.length()) return false;
+
+        if(dp[i][j] != -1) return dp[i][j];
+        //MAIN CASE
+        bool currMatch = (i < s.length()) && (p[j] == '.' || s[i]==p[j]);
+        bool ans;
+        if(j+1 < p.length() && p[j+1] == '*'){
+            bool emptyCase = solveMem(s,p,i,j+2,dp);//jab empty le rhe ho
+            bool elementCase = currMatch && solveMem(s,p,i+1,j,dp);//jab preceding element imagine kar rhe ho fir currMatch bhi check karo 
+            ans = emptyCase || elementCase;
+        }
+        else if(currMatch){
+            ans = solveMem(s,p,i+1,j+1,dp);
+        }
+        else{
+            ans = false;
+        }
+        dp[i][j] = ans;
+        return dp[i][j];
+    }
     bool isMatch(string s, string p) {
-        return solveRec(s,p,0,0);
+        // return solveRec(s,p,0,0);
+        int m = s.length();
+        int n = p.length();
+        vector<vector<int>> dp(m+1,vector<int>(n+1, -1));
+        return solveMem(s,p,0,0,dp);
+
     }
 };
