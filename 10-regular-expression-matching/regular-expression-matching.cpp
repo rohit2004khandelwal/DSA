db2de7ce987b8f00332b77dc31bf23b6d22d5eda
Rohit Khandelwal <159414490+rohit2004khandelwal@users.noreply.github.com>
2025-07-27 15:48:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (75.35%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -41,12 +41,66 @@ public:
         dp[i][j] = ans;
         return dp[i][j];
     }
+    bool solveTab(string&s, string&p, int m, int n){
+        vector<vector<int> > dp(m+1, vector<int>(n+1,0));
+        dp[m][n] = true;
+        for(int i = m; i>=0;i--){
+            for(int j = n-1; j>=0;j--){
+                bool currMatch = (i<s.length() && p[j] == '.' || s[i] == p[j]);
+                bool ans;
+                if(j+1 < p.length() && p[j+1] == '*'){
+                    bool emptyCase = dp[i][j+2];
+                    bool precElementCase = currMatch && dp[i+1][j];
+                    ans = emptyCase || precElementCase;
+                }
+                else if(currMatch){
+                    ans = dp[i+1][j+1];
+                }
+                else{
+                    ans = false;
+                }
+                dp[i][j] = ans;
+            }
+        }
+        return dp[0][0];
+    }
+    //SPACE OPT
+    bool solveTabSO(string&s, string&p, int m, int n){
+        vector<int>curr(n+1,0);
+        vector<int>next(n+1,0);
+        next[n] = true;
+        for(int i = m; i>=0;i--){
+            // if(i==m) curr[n] = true;
+            // else curr[n] = false;
+            curr[n] = (i==m); //important
+            for(int j = n-1; j>=0;j--){
+                bool currMatch = (i<s.length()) && (p[j] == '.' || s[i] == p[j]);
+                bool ans;
+                if(j+1 < p.length() && p[j+1] == '*'){
+                    bool emptyCase = curr[j+2];
+                    bool precElementCase = currMatch && next[j];
+                    ans = emptyCase || precElementCase;
+                }
+                else if(currMatch){
+                    ans = next[j+1];
+                }
+                else{
+                    ans = false;
+                }
+                curr[j] = ans;
+            }
+            next = curr;
+        }
+        return next[0];
+    }
     bool isMatch(string s, string p) {
         // return solveRec(s,p,0,0);
         int m = s.length();
         int n = p.length();
-        vector<vector<int>> dp(m+1,vector<int>(n+1, -1));
-        return solveMem(s,p,0,0,dp);
+        // vector<vector<int>> dp(m+1,vector<int>(n+1, -1));
+        // return solveMem(s,p,0,0,dp);
+        // return solveTab(s,p,m,n);
+        return solveTabSO(s,p,m,n);
 
     }
 };
