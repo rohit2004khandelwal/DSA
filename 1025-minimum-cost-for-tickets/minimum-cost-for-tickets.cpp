3eca081c8244d79c08582317777e823376e9fcc7
Rohit Khandelwal <159414490+rohit2004khandelwal@users.noreply.github.com>
2025-07-28 23:27:03 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (65.27%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -39,10 +39,32 @@ public:
         dp[i] = min({cost1, cost7, cost30});
         return dp[i];
     }
+    //BOTTOM UP
+    int solveTab(vector<int>& days, vector<int>& costs, int n){
+        vector<int> dp(n+1, 0);
+
+        for(int k=n-1;k>=0;k--){
+            int cost1 = costs[0] + dp[k + 1];
+
+        // 7 DAY PASS
+        int j = k;
+        while (j < n && days[j] < days[k] + 7) j++;
+        int cost7 = costs[1] + dp[j];
+
+        // 30 DAY PASS
+        j = k;
+        while (j < n && days[j] < days[k] + 30) j++;
+        int cost30 = costs[2] + dp[j];
+
+        dp[k] = min({cost1, cost7, cost30});
+        }
+        return dp[0];
+    }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         int n = days.size();
         // return solveRec(days, costs, n, 0);
-        vector<int> dp(n+1, -1);
-        return solveMem(days, costs, n, 0, dp);
+        // vector<int> dp(n+1, -1);
+        // return solveMem(days, costs, n, 0, dp);
+        return solveTab(days, costs, n);
     }
 };
