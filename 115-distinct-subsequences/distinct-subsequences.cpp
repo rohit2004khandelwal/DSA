2695f75b11a7fc38138c803eb5e188443cdff054
Rohit Khandelwal <159414490+rohit2004khandelwal@users.noreply.github.com>
2025-07-30 22:34:22 +0530

    Time: 31 ms (54.38%) | Memory: 44.4 MB (14.42%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,35 +1,61 @@
 class Solution {
-public: 
-    int solveRec(string &s, string &t, int i, int j){
-        //BASE CASE
-        if(j == t.length()) return 1;
-        if(i == s.length()) return 0;
-        //MAIN 
+public:
+    int solveRec(string& s, string& t, int i, int j) {
+        // BASE CASE
+        if (j == t.length())
+            return 1;
+        if (i == s.length())
+            return 0;
+        // MAIN
         int ans = 0;
-        if(s[i] == t[j]){
-            ans += solveRec(s, t, i+1, j+1);
+        if (s[i] == t[j]) {
+            ans += solveRec(s, t, i + 1, j + 1);
         }
-        ans += solveRec(s, t, i+1, j);
+        ans += solveRec(s, t, i + 1, j);
         return ans;
     }
-    //TOP DOWN
-    int solveMem(string &s, string &t, int i, int j, vector<vector<int>> &dp){
-        //BASE CASE
-        if(j == t.length()) return 1;
-        if(i == s.length()) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        //MAIN 
+    // TOP DOWN
+    int solveMem(string& s, string& t, int i, int j, vector<vector<int>>& dp) {
+        // BASE CASE
+        if (j == t.length())
+            return 1;
+        if (i == s.length())
+            return 0;
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        // MAIN
         int ans = 0;
-        if(s[i] == t[j]){
-            ans += solveMem(s, t, i+1, j+1, dp);
+        if (s[i] == t[j]) {
+            ans += solveMem(s, t, i + 1, j + 1, dp);
         }
-        ans += solveMem(s, t, i+1, j, dp);
+        ans += solveMem(s, t, i + 1, j, dp);
         dp[i][j] = ans;
         return dp[i][j];
     }
+    // BOTTOM UP
+    int solveTab(string& s, string& t) {
+        vector<vector<double>> dp(s.length() + 1, vector<double>(t.length() + 1, 0));
+        // BASE CASE ANALYSIS
+        for (int i = 0; i <= s.length(); i++) {
+            dp[i][t.length()] = 1;
+        }
+        // MAIN LOGIC
+        for (int i = s.length() - 1; i >= 0; i--) {
+            for (int j = t.length() - 1; j >= 0; j--) {
+                double ans = 0.0;
+                if (s[i] == t[j]) {
+                    ans += dp[i + 1][j + 1];
+                }
+                ans += dp[i+1][j];
+                dp[i][j] = ans;
+            }
+        }
+        return (int)dp[0][0];
+    }
     int numDistinct(string s, string t) {
         // return solveRec(s, t, 0, 0);
-        vector<vector<int>> dp(s.length()+1, vector<int>(t.length()+1, -1));
-        return solveMem(s, t, 0, 0, dp);
+        // vector<vector<int>> dp(s.length()+1, vector<int>(t.length()+1, -1));
+        // return solveMem(s, t, 0, 0, dp);
+        return solveTab(s, t);
     }
 };
