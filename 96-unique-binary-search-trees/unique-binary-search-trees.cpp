5fcac3cb5e01022f1c64ad4f6dd451963f8c4e4f
Rohit Khandelwal <159414490+rohit2004khandelwal@users.noreply.github.com>
2025-08-04 01:09:40 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (61.29%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -1,33 +1,36 @@
 class Solution {
-public: 
-    int solveRec(int n){
-        if(n <= 1) return 1;
+public:
+    int solveRec(int n) {
+        if (n <= 1)
+            return 1;
         int ans = 0;
-        for(int i=1;i<=n;i++){
-            ans += solveRec(i-1) * solveRec(n-i);
+        for (int i = 1; i <= n; i++) {
+            ans += solveRec(i - 1) * solveRec(n - i);
         }
         return ans;
     }
-    //TOP down
-    int solveMem(int n, vector<int> &dp){
-        if(n <= 1) return 1;
-        if(dp[n] != -1) return dp[n];
+    // TOP down
+    int solveMem(int n, vector<int>& dp) {
+        if (n <= 1)
+            return 1;
+        if (dp[n] != -1)
+            return dp[n];
         int ans = 0;
-        for(int i=1;i<=n;i++){
-            ans += solveMem(i-1, dp) * solveMem(n-i, dp);
+        for (int i = 1; i <= n; i++) {
+            ans += solveMem(i - 1, dp) * solveMem(n - i, dp);
         }
         dp[n] = ans;
         return dp[n];
     }
-    //BOTTOM UP
-    int solveTab(int n){
-        vector<int> dp(n+1, 0);
+    // BOTTOM UP
+    int solveTab(int n) {
+        vector<int> dp(n + 1, 0);
         dp[0] = dp[1] = 1;
-        //i->no of nodes in memo n is used for no of node
-        for(int i=2;i<=n;i++){ //2 bcoz 0 and 1 counted in base case
-            //j -> root node in memo i is used for root node
-            for(int j=1;j<=i;j++){
-                dp[i] += dp[j-1] * dp[i-j];
+        // i->no of nodes in memo n is used for no of node
+        for (int i = 2; i <= n; i++) { // 2 bcoz 0 and 1 counted in base case
+            // j -> root node in memo i is used for root node
+            for (int j = 1; j <= i; j++) {
+                dp[i] += dp[j - 1] * dp[i - j];
             }
         }
         return dp[n];
@@ -36,6 +39,11 @@ public:
         // return solveRec(n);
         // vector<int> dp(n+1, -1);
         // return solveMem(n, dp);
-        return solveTab(n);
+        // return solveTab(n);
+        // USING CATALAN NUMBER
+        long double ans = 1;
+        for (int k = 2; k <= n; k++)
+            ans *= (n + k), ans /= k;
+        return ans;
     }
 };
