d1f0685f889de463f80dd29ecf0d9cae9c0536e8
Rohit Khandelwal <159414490+rohit2004khandelwal@users.noreply.github.com>
2025-08-03 23:27:42 +0530

    Time: 2375 ms (5.01%) | Memory: 7.9 MB (61.29%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -8,7 +8,19 @@ public:
         }
         return ans;
     }
+    //TOP down
+    int solveMem(int n, vector<int> &dp){
+        if(n <= 1) return 1;
+        int ans = 0;
+        for(int i=1;i<=n;i++){
+            ans += solveMem(i-1, dp) * solveMem(n-i, dp);
+        }
+        dp[n] = ans;
+        return dp[n];
+    }
     int numTrees(int n) {
-        return solveRec(n);
+        // return solveRec(n);
+        vector<int> dp(n+1, -1);
+        return solveMem(n, dp);
     }
 };
