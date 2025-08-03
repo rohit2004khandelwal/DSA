add2d94f7a13e91a03b866119b4a5ca9f4d50876
Rohit Khandelwal <159414490+rohit2004khandelwal@users.noreply.github.com>
2025-08-03 22:59:11 +0530

    Time: 83 ms (33.19%) | Memory: 128.5 MB (51.06%) - LeetSync

---
+++ 2229-maximum-fruits-harvested-after-at-most-k-steps/maximum-fruits-harvested-after-at-most-k-steps.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int maxTotalFruits(vector<vector<int>>& fruits, int startPos, int k) {
+        int n = fruits.size();
+        vector<int> prefixSum(n);
+        vector<int> indices(n);
+        //BUILD PREFIX SUM AND EXTRACT INDICES
+        for(int i=0;i<n;i++){
+            indices[i] = fruits[i][0];
+            prefixSum[i] = fruits[i][1] + (i>0 ? prefixSum[i-1] : 0);
+        }
+        int maxFruits = 0;
+        for(int d=0;d<=k/2;d++){
+            //MOVE LEFT THEN RIGHT
+            int remain = k-2*d;
+            int i = startPos - d;
+            int j = startPos + remain;
+            int left = lower_bound(indices.begin(), indices.end(), i) - indices.begin();
+            int right = upper_bound(indices.begin(), indices.end(), j) - indices.begin() - 1;
+            if(left <= right){
+                int result = prefixSum[right] - (left > 0 ? prefixSum[left-1] : 0);
+                maxFruits = max(maxFruits, result);
+            }
+            //MOVE RIGHT THEN LEFT
+            i  = startPos - remain;
+            j  = startPos + d;
+            
+            left  = lower_bound(indices.begin(), indices.end(), i) - indices.begin();
+            right = upper_bound(indices.begin(), indices.end(), j) - indices.begin() - 1;
+
+            if(left <= right) {
+                int total = prefixSum[right] - (left > 0 ? prefixSum[left - 1] : 0);
+                maxFruits = max(maxFruits, total);
+            }
+        }
+        return maxFruits;
+    }
+};
