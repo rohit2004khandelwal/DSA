5b04e80cb518366717dfa9f27dcadb83e1fbd5b0
Rohit Khandelwal <159414490+rohit2004khandelwal@users.noreply.github.com>
2025-08-03 23:25:14 +0530

    Time: 2031 ms (9.47%) | Memory: 7.7 MB (89.41%) - LeetSync

---
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public: 
+    int solveRec(int n){
+        if(n <= 1) return 1;
+        int ans = 0;
+        for(int i=1;i<=n;i++){
+            ans += solveRec(i-1) * solveRec(n-i);
+        }
+        return ans;
+    }
+    int numTrees(int n) {
+        return solveRec(n);
+    }
+};
