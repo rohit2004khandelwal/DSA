c25b4beed2377e9554ebf9911497449a70fe7ee1
Rohit Khandelwal <159414490+rohit2004khandelwal@users.noreply.github.com>
2025-08-08 20:41:11 +0530

    Time: 65 ms (24.66%) | Memory: 31.1 MB (13.73%) - LeetSync

---
+++ 1240-stone-game-ii/stone-game-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int solveRec(vector<int>& piles, int i, int M, int aliceTurn){
+        if(i == piles.size()) return 0;
+        int ans = aliceTurn ? INT_MIN : INT_MAX;
+        int total = 0;
+        for(int X = 1; X <= 2 * M; X++){
+            if(i + X - 1 >= piles.size()) break;
+            total += piles[i + X - 1];
+            if(aliceTurn) ans = max(ans, total + solveRec(piles, i + X, max(X, M), !aliceTurn));
+            else ans = min(ans, solveRec(piles, i + X, max(X, M), !aliceTurn));
+        }
+        return ans;
+    }
+    //TOP DOWN
+    int solveMem(vector<int>& piles, int i, int M, int alice, vector<vector<vector<int>>> &dp){
+        if(i == piles.size()) return 0;//BASE CASE
+        if(dp[i][M][alice] != -1) return dp[i][M][alice];
+        int ans = alice ? INT_MIN : INT_MAX;
+        int total = 0;
+        for(int X = 1; X <= 2 * M; X++){
+            if(i + X - 1 >= piles.size()) break;
+            total += piles[i + X - 1];
+            if(alice) ans = max(ans, total + solveMem(piles, i + X, max(X, M), !alice, dp));
+            else ans = min(ans, solveMem(piles, i + X, max(X, M), !alice, dp));
+        }
+        return dp[i][M][alice] = ans;
+    }
+    int stoneGameII(vector<int>& piles) {
+        // return solveRec(piles, 0, 1, true);
+        //3D DP USED HERE
+        vector<vector<vector<int>>> dp(piles.size()+1, vector<vector<int>>(piles.size()+1, vector<int>(2, -1)));
+        return solveMem(piles, 0, 1, true, dp);
+    }
+};
