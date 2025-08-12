0feeb8256f377d40b582f440992f738edd1624cb
Rohit Khandelwal <159414490+rohit2004khandelwal@users.noreply.github.com>
2025-08-12 23:14:19 +0530

    Time: 29 ms (68.31%) | Memory: 31.7 MB (15.41%) - LeetSync

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int solveRec(string &s, int i, int j){
+        if(i>=j) return 0;
+        //RECURSION
+        if(s[i] == s[j]){
+            return solveRec(s, i+1, j-1);
+        }
+        else{
+            return 1 + min(solveRec(s, i+1, j), solveRec(s, i, j-1));
+        }
+    }
+    //TOP DOWN
+    int solveMem(string &s, int i, int j, vector<vector<int>> &dp){
+        if(i>=j) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        //RECURSION
+        int ans = 0;
+        if(s[i] == s[j]){
+            ans = solveMem(s, i+1, j-1, dp);
+        }
+        else{
+            ans = 1 + min(solveMem(s, i+1, j, dp), solveMem(s, i, j-1, dp));
+        }
+        return dp[i][j] = ans;
+    }
+    int minInsertions(string s) {
+        // return solveRec(s, 0, s.size()-1);
+        vector<vector<int>> dp(s.size()+1, vector<int>(s.size()+1, -1));
+        return solveMem(s, 0, s.size()-1, dp);
+    }
+};
