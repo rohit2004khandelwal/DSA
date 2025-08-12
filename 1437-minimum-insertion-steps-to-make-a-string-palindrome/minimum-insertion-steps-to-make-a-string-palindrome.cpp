3e5bbaf30addadef5232a5760f699992a1b13293
Rohit Khandelwal <159414490+rohit2004khandelwal@users.noreply.github.com>
2025-08-12 23:24:15 +0530

    Time: 24 ms (74.73%) | Memory: 31.5 MB (37.55%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -24,9 +24,27 @@ public:
         }
         return dp[i][j] = ans;
     }
+    //BOTTOM UP
+    int solveTab(string &s){
+        vector<vector<int>> dp(s.size()+1, vector<int>(s.size()+1, 0));
+        for(int i=s.size()-1;i>=0;i--){
+            for(int j=i+1;j<=s.size()-1;j++){
+                int ans = 0;
+                if(s[i] == s[j]){
+                    ans = dp[i+1][j-1];
+                }
+                else{
+                    ans = 1 + min(dp[i+1][j], dp[i][j-1]);
+                }
+                dp[i][j] = ans;
+            }
+        }
+        return dp[0][s.size()-1];
+    }
     int minInsertions(string s) {
         // return solveRec(s, 0, s.size()-1);
-        vector<vector<int>> dp(s.size()+1, vector<int>(s.size()+1, -1));
-        return solveMem(s, 0, s.size()-1, dp);
+        // vector<vector<int>> dp(s.size()+1, vector<int>(s.size()+1, -1));
+        // return solveMem(s, 0, s.size()-1, dp);
+        return solveTab(s);
     }
 };
