87af8db94304ad3ac1da3370d47bbd84d7b8f9b7
Rohit Khandelwal <159414490+rohit2004khandelwal@users.noreply.github.com>
2025-08-13 23:13:40 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (14.61%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -1,10 +1,16 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n<=0) return false;
-        while(n%3 == 0){
-            n/=3;
-        }
-        return n==1;
+        // if(n<=0) return false;
+        // while(n%3 == 0){
+        //     n/=3;
+        // }
+        // return n==1;
+        //APPROACH 3
+        // if(n<=0) return false;
+        // double x = log10(n) / log10(3);
+        // return x == (int)x;
+        //APPROACH 4
+        return n>0 && 1162261467%n == 0;
     }
 };
