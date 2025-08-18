5e2c0c96b31b0a2e9264643a20fb404aac1f09db
Rohit Khandelwal <159414490+rohit2004khandelwal@users.noreply.github.com>
2025-08-18 22:18:49 +0530

    Time: 102 ms (65.08%) | Memory: 79.4 MB (85.48%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -60,10 +60,37 @@ public:
         }
         return dp[0][1][2];
     }
+    //SPACE OPT
+    int solveTabSO(vector<int>& prices){
+        vector<vector<int>> curr(2, vector<int>(3, 0));
+        vector<vector<int>> next(2, vector<int>(3, 0));
+        
+        for(int i=prices.size()-1; i>=0; i--){
+            for(int buy=0; buy<=1; buy++){
+                for(int limit=1; limit<=2; limit++){ //LIMIT 0 WALA CASE ALREADY HANDLE HO CHUKA HAI
+                    int profit = 0;
+                    if(buy){
+                        int buyKaro = -prices[i] + next[0][limit];
+                        int skipKaro = 0 + next[1][limit];
+                        profit = max(buyKaro, skipKaro);
+                    }
+                    else{
+                        int sellKaro = prices[i] + next[1][limit-1];//SELL HONE PAR HI EK TRANSACTION KHTM HOGI ISLIYE SELL PAR LIMIT-1 LARNA HAI
+                        int skipKaro = 0 + next[0][limit];
+                        profit = max(sellKaro, skipKaro);
+                    }
+                    curr[buy][limit] = profit;
+                }
+                next = curr;
+            }
+        }
+        return next[1][2];
+    }
     int maxProfit(vector<int>& prices) {
         // return solveRec(0, 1, 2, prices);
         // vector<vector<vector<int>>> dp(prices.size()+1, vector<vector<int>>(2, vector<int>(3, -1)));
         // return solveMem(0, 1, 2, prices, dp);
-        return solveTab(prices);
+        // return solveTab(prices);
+        return solveTabSO(prices);
     }
 };
