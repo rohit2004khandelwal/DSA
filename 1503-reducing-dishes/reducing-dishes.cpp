0074460d27aa7646beca46719814cee4c3a6e958
Rohit Khandelwal <159414490+rohit2004khandelwal@users.noreply.github.com>
2025-08-21 23:51:07 +0530

    Time: 14 ms (43.46%) | Memory: 25.1 MB (15.72%) - LeetSync

--- 1503-reducing-dishes/reducing-dishes.cpp
+++ 1503-reducing-dishes/reducing-dishes.cpp
@@ -6,7 +6,7 @@ public:
         int excludeLikeTimeCoeff = solve(sat, i + 1, time);
         return max(includeLikeTimeCoeff, excludeLikeTimeCoeff);
     }
-    
+
      int solveTD(vector<int>& sat, int i, int time, vector<vector<int>>& dp){
         if(i == sat.size()) return 0;
         if(dp[i][time] != -1) return dp[i][time];
@@ -14,9 +14,21 @@ public:
         int excludeLikeTimeCoeff = solveTD(sat, i + 1, time, dp);
         return dp[i][time] = max(includeLikeTimeCoeff, excludeLikeTimeCoeff);
     }
+    int solveBU(vector<int>& sat){
+        vector<vector<int>>dp(sat.size() + 1, vector<int>(sat.size() + 2, 0));
+        for(int i = sat.size() - 1; i >= 0; i--){
+            for(int j = sat.size(); j >= 1; j--){
+                int includeLikeTimeCoeff = j * sat[i] + dp[i + 1][j + 1];
+                int excludeLikeTimeCoeff = dp[i + 1][j];
+                dp[i][j] = max(includeLikeTimeCoeff, excludeLikeTimeCoeff);
+            }
+        }
+        return dp[0][1];
+    }
     int maxSatisfaction(vector<int>& satisfaction) {
          sort(satisfaction.begin(), satisfaction.end());
-         vector<vector<int>>dp(satisfaction.size(), vector<int>(satisfaction.size() + 1, -1));
-         return solveTD(satisfaction, 0, 1, dp);
+        //  vector<vector<int>>dp(satisfaction.size(), vector<int>(satisfaction.size() + 1, -1));
+        //  return solveTD(satisfaction, 0, 1, dp);
+          return solveBU(satisfaction);
     }
 };
