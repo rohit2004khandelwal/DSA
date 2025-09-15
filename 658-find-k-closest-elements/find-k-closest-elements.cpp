6151ff5f701c732d3b88d7ff16ef2cc5db67a191
Rohit Khandelwal <159414490+rohit2004khandelwal@users.noreply.github.com>
2025-09-15 22:49:12 +0530

    Time: 0 ms (100.00%) | Memory: 36.4 MB (52.89%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/find-k-closest-elements.cpp
@@ -2,14 +2,24 @@ class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
         //CUSTOM COMPARATOR
-        sort(arr.begin(), arr.end(), [x](int a, int b){
-            if(abs(a-x) == abs(b-x)){
-                return a < b;
-            }
-            return abs(a-x) < abs(b-x);
-        });
-        vector<int> ans(arr.begin(), arr.begin()+k);
-        sort(ans.begin(), ans.end());
+        // sort(arr.begin(), arr.end(), [x](int a, int b){
+        //     if(abs(a-x) == abs(b-x)){
+        //         return a < b;
+        //     }
+        //     return abs(a-x) < abs(b-x);
+        // });
+        // vector<int> ans(arr.begin(), arr.begin()+k);
+        // sort(ans.begin(), ans.end());
+        // return ans;
+        int l=0,h=arr.size()-1;
+        while(h-l>=k){
+            if(x-arr[l] > arr[h]-x) l++;
+            else h--;
+        }
+        vector<int> ans;
+        for(int i=l;i<=h;i++){
+            ans.push_back(arr[i]);
+        }
         return ans;
     }
 };
