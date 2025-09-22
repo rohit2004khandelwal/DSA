0e4f99a30a0f6845a00143443ed62fefbfcf4673
Rohit Khandelwal <159414490+rohit2004khandelwal@users.noreply.github.com>
2025-09-22 10:06:48 +0530

    Time: 0 ms (100.00%) | Memory: 23 MB (97.44%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.cpp
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.cpp
@@ -1,20 +1,42 @@
+// class Solution {
+// public:
+//     int maxFrequencyElements(vector<int>& nums) {
+//         int n = nums.size();
+//         int result = 0;
+//         int maxFreq = 0;
+//         unordered_map<int, int> freq;
+//         for(int num:nums){
+//             freq[num]++;
+//             maxFreq = max(maxFreq, freq[num]);
+//         }
+//         int sum=0;
+//         for(auto &x : freq){
+//             if(x.second == maxFreq){
+//                 sum += x.second;
+//             }
+//         }
+//         return sum;
+//     }
+// };
+// -------------------------------------------------------------------
 class Solution {
 public:
     int maxFrequencyElements(vector<int>& nums) {
         int n = nums.size();
-        int result = 0;
+        int count = 0;
         int maxFreq = 0;
         unordered_map<int, int> freq;
         for(int num:nums){
             freq[num]++;
-            maxFreq = max(maxFreq, freq[num]);
-        }
-        int sum=0;
-        for(auto &x : freq){
-            if(x.second == maxFreq){
-                sum += x.second;
+            int curFreq = freq[num];
+            if(curFreq > maxFreq){
+                maxFreq = curFreq;
+                count = maxFreq;
+            }
+            else if(curFreq == maxFreq){
+                count += maxFreq;
             }
         }
-        return sum;
+        return count;
     }
 };
