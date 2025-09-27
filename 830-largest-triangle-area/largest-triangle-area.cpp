af90be70d94a14b30f60a775083692960d3a85f9
Rohit Khandelwal <159414490+rohit2004khandelwal@users.noreply.github.com>
2025-09-27 22:58:27 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (34.38%) - LeetSync

--- 830-largest-triangle-area/largest-triangle-area.cpp
+++ 830-largest-triangle-area/largest-triangle-area.cpp
@@ -10,8 +10,15 @@ public:
                     double x2 = points[j][0], y2 = points[j][1];
                     double x3 = points[k][0], y3 = points[k][1];
 
-                    double area = 0.5 * abs(x1*(y2-y3) + x2*(y3-y1) + x3*(y1-y2));
+                    double area = 0.5 * abs(x1*(y2-y3) + x2*(y3-y1) + x3*(y1-y2));  //REMEMBER IT IS Y3-Y1 NOT VICE VERSA
                     maxArea = max(maxArea, area);
+                    //HERONS FORMULA
+                    // double a = hypot(x2-x1,y2-y1);
+                    // double b = hypot(x2-x3,y2-y3);
+                    // double c = hypot(x3-x1,y3-y1);
+
+                    // double s = (a+b+c) / 0.5;
+                    // double herons = sqrt(s * (s-a) * (s-b) * (s-c));
 
                 }
             }
