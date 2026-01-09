2117f0c56658165d72656dc4e7839743564bbbe9
Rohit Khandelwal <159414490+rohit2004khandelwal@users.noreply.github.com>
2026-01-09 23:35:06 +0530

    Time: 3 ms (22.56%) | Memory: 23 MB (19.48%) - LeetSync

---
+++ 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    unordered_map<int, int> mp;
+    int maxD = 0;
+    TreeNode* LCA(TreeNode* root){
+        if(root == NULL) return NULL;
+        if(mp[root->val] == maxD) return root;
+
+        TreeNode* leftAns = LCA(root->left);
+        TreeNode* rightAns = LCA(root->right);
+
+        if(leftAns == NULL && rightAns == NULL) return NULL;
+        else if(leftAns == NULL && rightAns != NULL) return rightAns;
+        else if(leftAns != NULL && rightAns == NULL) return leftAns;
+        else{
+            // both leftAns and rightAns are non null
+            return root;
+        }
+    }
+    void depth(TreeNode* root, int d){
+        if(root == NULL) return;
+        maxD = max(maxD, d);
+        mp[root -> val] = d;
+        depth(root->left, d+1);
+        depth(root->right, d+1);
+    }
+    TreeNode* lcaDeepestLeaves(TreeNode* root) {
+        depth(root, 0);  
+        return LCA(root);  
+    }
+};
