6f6061b6280239b9cb7a37428df70849c1dce076
Rohit Khandelwal <159414490+rohit2004khandelwal@users.noreply.github.com>
2026-01-09 23:58:23 +0530

    Time: 0 ms (100.00%) | Memory: 16.1 MB (80.11%) - LeetSync

--- 896-smallest-subtree-with-all-the-deepest-nodes/smallest-subtree-with-all-the-deepest-nodes.cpp
+++ 896-smallest-subtree-with-all-the-deepest-nodes/smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -11,32 +11,16 @@
  */
 class Solution {
 public:
-unordered_map<int, int> mp;
-    int maxD = 0;
-    TreeNode* LCA(TreeNode* root){
-        if(root == NULL) return NULL;
-        if(mp[root->val] == maxD) return root;
+    pair<int, TreeNode*> solve(TreeNode* root){
+        if(root == NULL) return {0, NULL};
+        auto l = solve(root -> left);
+        auto r = solve(root -> right);
 
-        TreeNode* leftAns = LCA(root->left);
-        TreeNode* rightAns = LCA(root->right);
-
-        if(leftAns == NULL && rightAns == NULL) return NULL;
-        else if(leftAns == NULL && rightAns != NULL) return rightAns;
-        else if(leftAns != NULL && rightAns == NULL) return leftAns;
-        else{
-            // both leftAns and rightAns are non null
-            return root;
-        }
-    }
-    void depth(TreeNode* root, int d){
-        if(root == NULL) return;
-        maxD = max(maxD, d);
-        mp[root -> val] = d;
-        depth(root->left, d+1);
-        depth(root->right, d+1);
+        if(l.first == r.first) return {l.first+1, root};
+        else if(l.first > r.first) return {l.first+1, l.second};
+        else return {r.first+1, r.second};
     }
     TreeNode* subtreeWithAllDeepest(TreeNode* root) {
-        depth(root, 0);  
-        return LCA(root);
+        return solve(root).second;
     }
 };
