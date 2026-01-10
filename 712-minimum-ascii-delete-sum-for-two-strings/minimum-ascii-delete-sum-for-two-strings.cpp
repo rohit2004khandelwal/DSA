3d15e81fd2534637e19e33f48140ec31f8728916
Rohit Khandelwal <159414490+rohit2004khandelwal@users.noreply.github.com>
2026-01-10 16:05:25 +0530

    Time: 45 ms (8.93%) | Memory: 18.3 MB (49.34%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
@@ -37,11 +37,33 @@ public:
         dp[i][j] = ans;
         return dp[i][j];
     }
+    //BOTTOM UP
+    int solveTab(string &s1, string &s2, int i, int j){
+        vector<vector<int>> dp(i+1, vector<int>(j+1, 0));
+        int ans;
+        if(i >= s1.length() && j >= s2.length()) return 0;
+        if(i >= s1.length()){
+            return s2[j] + solveMem(s1, s2, i, j+1, dp);
+        }
+        if(j >= s2.length()){
+            return s1[i] + solveMem(s1, s2, i+1, j, dp);
+        }
+        if(dp[i][j] != -1) return dp[i][j];
+        if(s1[i] == s2[j]) ans = solveMem(s1, s2, i+1, j+1, dp);
+        else{
+            int delS1 = s1[i] + solveMem(s1, s2, i+1,j, dp);
+            int delS2 = s2[j] + solveMem(s1, s2, i, j+1, dp);
+            ans = min(delS1, delS2);
+        }
+        dp[i][j] = ans;
+        return dp[i][j];
+    }
     int minimumDeleteSum(string s1, string s2) {
         int i = s1.length();
         int j = s2.length();
         // return solveRec(s1, s2, 0, 0);
         vector<vector<int>> dp(i+1, vector<int>(j+1, -1));
         return solveMem(s1, s2, 0, 0, dp);
+        // return solveTab(s1, s2, 0, 0);
     }
 };
